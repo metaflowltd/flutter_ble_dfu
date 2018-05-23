4be85490dc16312dd27ed5be7888e92bc97c9fbd
zshema <zachi@metaflow.co>
2018-05-23 16:01:12 +0300

    clean duplicate

--- ios/Classes copy/BleDfuPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "BleDfuPlugin.h"
-#import <ble_dfu/ble_dfu-Swift.h>
-
-@implementation BleDfuPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftBleDfuPlugin registerWithRegistrar:registrar];
-}
-@end
